c237758f212dae993285ffd608cbdf11ce690b2b
webHero <khonjelwayo@gmail.com>
2021-06-09 19:15:44 +0200

    animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 export interface Card{
   title: string,
   description: string,
@@ -16,35 +17,36 @@ export class AppComponent implements OnInit {
   title = 'profile';
   slideIndex = 1;
   slideWidth: number = 608;
-
+  titleHeight: number = 131;
+  cardNo: number = 131;
 
   cards: Array<Card> = [
     {
-      title: 'Brandind',
+      title: 'Branding',
       description: 'Some quick example text to build on the card title and make up the bulk of the card content',
       src: '../assets/images/1.jpg',
       no: "01"
     },
     {
-      title: 'Branding',
+      title: 'Design',
       description: 'Some quick example text to build on the card title and make up the bulk of the card content',
       src: '../assets/images/2.jpg',
       no: "02"
     },
     {
-      title: 'Development',
+      title: 'Develop',
       description: 'Some quick example text to build on the card title and make up the bulk of the card content',
       src: '../assets/images/3.jpg',
       no: "03"
     },
     {
-      title: 'Translation',
+      title: 'Translate',
       description: 'Some quick example text to build on the card title and make up the bulk of the card content',
       src: '../assets/images/4.jpg',
       no: "04"
     },
     {
-      title: 'Deploying',
+      title: 'Deploy',
       description: 'Some quick example text to build on the card title and make up the bulk of the card content',
       src: '../assets/images/1.jpg',
       no: "05"
@@ -63,23 +65,23 @@ export class AppComponent implements OnInit {
   nextSlide(n){
     let dots: HTMLCollection = document.getElementsByClassName('dot');
     let elem: HTMLDivElement = document.querySelector('.slideshow-container');
+    let elem1: HTMLDivElement = document.querySelector("div.card-title.text-center.overflow-hidden > div")
+    let elem2: HTMLDivElement = document.querySelector("div.card-no.text-center.overflow-hidden.rounded > div")
+
     this.slideIndex = n  
     
     // console.log(dots);
     
     elem.style.left = `-${this.slideWidth * this.slideIndex}px`
+    elem1.style.bottom = `${this.titleHeight * this.slideIndex}px`
+    elem2.style.bottom = `${this.cardNo * this.slideIndex}px`
+    
 
     for (let i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
-      console.log(dots[i].className);
+      // console.log(dots[i].className);
       
-      // dots[i].classList.toggle('active')
     }    
   }
 
 }
-
-
-
-
-
