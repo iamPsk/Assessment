70556364a4611243bb41a94d9c7b0db7905d9be9
webHero <khonjelwayo@gmail.com>
2021-07-14 10:21:47 +0200

    init

---
+++ src/app/components/testomonials/testomonials.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TestomonialsComponent } from './testomonials.component';
+
+describe('TestomonialsComponent', () => {
+  let component: TestomonialsComponent;
+  let fixture: ComponentFixture<TestomonialsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TestomonialsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestomonialsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/testomonials/testomonials.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-testomonials',
+  templateUrl: './testomonials.component.html',
+  styleUrls: ['./testomonials.component.scss']
+})
+export class TestomonialsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
