7dd8602fa6c6860a3721628e2a122fd513c30207
webHero <khonjelwayo@gmail.com>
2021-07-14 10:25:54 +0200

    refactor moved to header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Card, Slide } from './models/interfaces';
+import { Card, Experience } from 'src/app/models/interfaces';
 
 @Component({
   selector: 'app-root',
@@ -9,35 +9,6 @@ import { Card, Slide } from './models/interfaces';
 
 export class AppComponent implements OnInit {
   title = 'profile';
-  slideIndex = 1;
-  slideWidth: number = 608;
-  titleHeight: number = 78;
-  cardNo: number = 165;
-
-  slides: Array<Slide> = [
-    {
-      img: '../assets/images/1.jpg',
-      title: 'Mobile Web development'
-    },
-    {
-      img: '../assets/images/2.jpg',
-      title: 'Server-side scripting'
-    },
-    {
-      img: '../assets/images/3.jpg',
-      title: 'Progressive Web Apps'
-    },
-    {
-      img: '../assets/images/1.jpg',
-      title: 'Web Content'
-    },
-    {
-      img: '../assets/images/WakeInternalMedicine-ResponsiveScreens-e1593623160290.png',
-      title: 'Mobile First Responsive Design'
-      
-    },
-  ]
-
   cards: Array<Card> = [
     {
       title: 'Mobile Web Applications',
@@ -89,51 +60,45 @@ export class AppComponent implements OnInit {
     //   We deliver quality solutions through selecting the best available technology tools to meet your specific need.
 
   ]
+  experiences: Array<Experience> = [
+    {
+      title: 'Student Mentor',
+      description: 'Lorem, consectetur adipisicing elit!',
+      company: 'Google Africa Developer Scholarship',
+      href: 'www.google.co.za'
+    },
+    {
+      title: 'web developer',
+      description: 'Lorem, consectetur adipisicing elit!',
+      company: 'SLOWCODE',
+      href: 'http://slowcode.co.za'
+    },
+    {
+      title: 'Front-end Developer',
+      description: 'Lorem, consectetur adipisicing elit!',
+      company: 'Mkoko Computers',
+      href: 'http://smart-pos.co.za'
+    },
+    {
+      title: 'Front-end Developer',
+      description: 'Lorem, consectetur adipisicing elit!',
+      company: 'Tick Works',
+      href: 'http://smart-pos.co.za'
+    },
+    {
+      title: 'Student Mentor',
+      description: 'Lorem, consectetur adipisicing elit!',
+      company: 'Google Africa Developer Scholarship',
+      href: 'http://smart-pos.co.za'
+    },
+  ]
+
 
   constructor() {
 
   }
 
-  ngOnInit() {
-    this.nextSlide(0)
-    // this.autoSlide()
-  }
-
-  autoSlide() {
-  }
-
-
-
-  nextSlide(n) {
-    // document.querySelector("body > app-root > header > section > div > div.col-10.p-0 > div > div.d-flex.position-relative > div.slide-title.text-center.overflow-hidden > div")
-    let dots: HTMLCollection = document.getElementsByClassName('dot');
-    let slideContainer: HTMLDivElement = document.querySelector('.slideshow-container');
-    let slideTitle: HTMLDivElement = document.querySelector(".slide-title.overflow-hidden > div")
-    let cardNo: HTMLDivElement = document.querySelector("div.card-no.text-center.overflow-hidden.rounded > div")
-
-    this.slideIndex = n
-
-    slideContainer.style.left = `-${this.slideWidth * this.slideIndex}px`
-    slideTitle.style.bottom = `${this.titleHeight * this.slideIndex}px`
-    cardNo.style.bottom = `${this.cardNo * this.slideIndex}px`
-
-
-
-    for (let i = 0; i < dots.length; i++) {
-
-      if (dots[i].classList.contains('active')) {
-        dots[i].classList.remove('active')
-      }
-
-
-      if (i == this.slideIndex) {
-        dots[i].classList.add('active')
-      }
-
-    }
-
-
+  ngOnInit() {  }
     
-  }
 
 }
