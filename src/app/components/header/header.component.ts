c0f0d557ed4f673188e967f98ed59cb504c4c69f
webHero <khonjelwayo@gmail.com>
2021-07-18 12:49:44 +0200

    mobile first responsive design

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { Slide, Card } from 'src/app/models/interfaces';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Slide } from 'src/app/models/interfaces';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+
+export class HeaderComponent implements OnInit, AfterViewInit {
   slideIndex = 1;
   slideWidth: number = 608;
-  titleHeight: number = 78;
+  titleHeight: number = 75;
   cardNo: number = 165;
 
   slides: Array<Slide> = [
@@ -32,32 +33,35 @@ export class HeaderComponent implements OnInit {
     {
       img: '../assets/images/WakeInternalMedicine-ResponsiveScreens-e1593623160290.png',
       title: 'Mobile First Responsive Design'
-      
+
     },
   ]
 
-
-
-  constructor() { }
-
+  constructor() { 
+  }
+  
   ngOnInit(): void {
+    // this.nextSlide(0);
+  }
+  
+  ngAfterViewInit(): void {
     this.nextSlide(0)
+    this.activateObserver()
   }
 
   nextSlide(n) {
-    // document.querySelector("body > app-root > header > section > div > div.col-10.p-0 > div > div.d-flex.position-relative > div.slide-title.text-center.overflow-hidden > div")
+    let titleH1: HTMLElement = document.querySelector("body > app-root > app-header > header > section > div > div.col-lg-10.p-0 > div > div.d-flex.position-relative > div.slide-title.overflow-hidden > div > h1:nth-child(1)")
+
     let dots: HTMLCollection = document.getElementsByClassName('dot');
     let slideContainer: HTMLDivElement = document.querySelector('.slideshow-container');
     let slideTitle: HTMLDivElement = document.querySelector(".slide-title.overflow-hidden > div")
     let cardNo: HTMLDivElement = document.querySelector("div.card-no.text-center.overflow-hidden.rounded > div")
-
+    
     this.slideIndex = n
-
     slideContainer.style.left = `-${this.slideWidth * this.slideIndex}px`
-    slideTitle.style.bottom = `${this.titleHeight * this.slideIndex}px`
+    // slideTitle.style.bottom = `${this.titleHeight * this.slideIndex}px`
+    slideTitle.style.bottom = `${(titleH1.offsetHeight + 8) * this.slideIndex}px`
     cardNo.style.bottom = `${this.cardNo * this.slideIndex}px`
-
-
 
     for (let i = 0; i < dots.length; i++) {
 
@@ -69,11 +73,26 @@ export class HeaderComponent implements OnInit {
       if (i == this.slideIndex) {
         dots[i].classList.add('active')
       }
-
     }
-
-
-    
   }
 
+  activateObserver(){
+    let titleH1: HTMLElement = document.querySelector("body > app-root > app-header > header > section > div > div.col-lg-10.p-0 > div > div.d-flex.position-relative > div.slide-title.overflow-hidden > div > h1")
+
+    // let titleSlider = document.querySelector("body > app-root > app-header > header > section > div > div.col-lg-10.p-0 > div > div.d-flex.position-relative > div.slide-title.overflow-hidden > div")
+
+    let resizeDiv = document.querySelector('body > app-root > app-header > header > section > div > div.col-lg-10.p-0 > div')
+
+    let sliderContainer:HTMLDivElement = document.querySelector("body > app-root > app-header > header > section > div > div.col-lg-10.p-0 > div > div.d-flex.position-relative > div.slide-title.overflow-hidden")
+
+    let it: HTMLElement = document.querySelector("body > app-root > app-header > header > section > div > div.col-lg-10.p-0 > div > div.d-flex.position-relative > div.position-relative.overflow-hidden > div > div:nth-child(1)")
+
+    const resizeObserver = new ResizeObserver(() => {
+      this.slideWidth = it.offsetWidth + 48
+      sliderContainer.style.height = `${titleH1.offsetHeight + 8}px`    
+      // console.log('sizechanged');
+    });
+
+    resizeObserver.observe(resizeDiv)
+  }
 }
